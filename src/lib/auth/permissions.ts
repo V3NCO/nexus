765f620f3504c33f42b22364aa9b8233a687d787
Venco <v3nco@v3nco.dev>
2025-12-15 21:45:12 +0100

    Auth fix

--- src/lib/auth/permissions.ts
+++ src/lib/auth/permissions.ts
@@ -1,26 +1,25 @@
-import { createAccessControl } from "better-auth/plugins/access";
-import { defaultStatements, adminAc } from "better-auth/plugins/admin/access";
+import { createAccessControl } from 'better-auth/plugins/access';
+import { defaultStatements, adminAc } from 'better-auth/plugins/admin/access';
 
-/**
- * make sure to use `as const` so typescript can infer the type correctly
- */
 const statement = {
-    location: ["read", "update"],
-    git: ["read"],
+	...adminAc.statements,
+	location: ['read', 'update'],
+	git: ['read']
 } as const;
 
 export const ac = createAccessControl(statement);
 
 export const user = ac.newRole({
-    ...defaultStatements,
-    git: ["read"],
+	user: [...defaultStatements.user],
+	session: [...defaultStatements.session],
+	git: ['read']
 });
 
 export const admin = ac.newRole({
-    ...adminAc.statements,
-    location: ["read", "update"],
+	...adminAc.statements,
+	location: ['read', 'update']
 });
 
 export const locationAccess = ac.newRole({
-    location: ["read"],
+	location: ['read']
 });
