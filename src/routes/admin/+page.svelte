86080b7748c63297250c30639e8ed934ec774b3f
Venco <v3nco@v3nco.dev>
2025-12-20 12:47:13 +0100

    start admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { authClient } from "$lib/auth/auth-client";
+  const availableRoles = ["user", "admin", "locationAccess"]
+</script>
+
+{#await authClient.admin.listUsers({
+  query: {
+    limit: 1000,
+    sortBy: "email"
+  }
+})}
+    <p>Loading Users...</p>
+{:then response}
+    {console.log(response)}
+{/await}
