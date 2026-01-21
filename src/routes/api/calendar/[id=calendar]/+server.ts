f4b8f460ad0b34cd5744954e154664ce34bc19bc
Venco <v3nco@v3nco.dev>
2026-01-21 21:30:39 +0100

    Readd some api endpoints, make the config color apply as fallback

--- src/routes/api/calendar/[id=calendar]/+server.ts
+++ src/routes/api/calendar/[id=calendar]/+server.ts
@@ -52,7 +52,7 @@ export async function GET({url, locals, params}: RequestEvent): Promise<Response
                   ) {
                     events.push({
                       "summary": ievent.summary,
-                      "color": ievent.color,
+                      "color": ievent.color || item.color,
                       //"location": ievent.location,
                       "start": {
                         year: eventStart.getUTCFullYear(),
@@ -92,4 +92,79 @@ export async function GET({url, locals, params}: RequestEvent): Promise<Response
     } catch (error: any) {
         return json({error: error.message}, {status: 500});
     }
+  }
+export async function PATCH({ locals, params, request }: RequestEvent): Promise<Response> {
+	const user = locals.user;
+	if (!user) {
+		return json({ error: 'Unauthorized' }, { status: 401 });
+	}
+
+	const canWriteCalendar = await auth.api.userHasPermission({
+		body: {
+			userId: user.id,
+			permission: { calendar: ['write'] } as any
+		}
+	});
+
+	if (!canWriteCalendar.success) {
+		return json({ error: 'Forbidden' }, { status: 403 });
+	}
+
+	try {
+		const body = await request.json();
+    const { url, color, name, username, password } = body;
+
+		const [updatedCalendar] = await db
+			.update(calendars)
+			.set({
+				...(url && { url }),
+				...(color && { color }),
+        ...(name && { name }),
+        ...(username && { username }),
+        ...(password && { password })
+			})
+			.where(eq(calendars.id, params.id!))
+			.returning();
+
+		if (!updatedCalendar) {
+			return json({ error: 'Calendar not found' }, { status: 404 });
+		}
+
+		return json(updatedCalendar);
+	} catch (error: any) {
+		return json({ error: error.message }, { status: 500 });
+	}
+    }
+
+export async function DELETE({ locals, params }: RequestEvent): Promise<Response> {
+  const user = locals.user;
+  if (!user) {
+    return json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const canDeleteCalendar = await auth.api.userHasPermission({
+    body: {
+      userId: user.id,
+      permission: { calendar: ['delete'] } as any
+    }
+  });
+
+  if (!canDeleteCalendar.success) {
+    return json({ error: 'Forbidden' }, { status: 403 });
+  }
+
+  try {
+    const [deletedCalendar] = await db
+      .delete(calendars)
+      .where(eq(calendars.id, params.id!))
+      .returning();
+
+    if (!deletedCalendar) {
+      return json({ error: 'Calendar not found' }, { status: 404 });
+    }
+
+    return json({ message: 'Calendar deleted successfully' });
+  } catch (error: any) {
+    return json({ error: error.message }, { status: 500 });
+  }
 }
